4e21e2a741c593d82feb807ae303c87c972656c1
Matt Kantor <the.matt.kantor@gmail.com>
2012-07-28 16:28:59 -0700

    Fixed a nasty bug where the timer for the next animate() iteration was created for every animated element (instead of once for all elements). Added an `abort` method and made the plugin store timeout IDs in $(...).data(). Simplified cubicBezierAtTime() a bit.

--- jquery.keyframeanimation.js
+++ jquery.keyframeanimation.js
@@ -11,14 +11,27 @@
 	
 	Author: Matt Kantor http://mattkantor.com
 	
-	Version: 0.2.0
+	Version: 0.3.1
 */
 
 // NOTE: "percentage" as used below refers to numbers between 0 and 1
 ;(function($, window, undefined) {
 	
-	$.fn.keyframeAnimation = function(settings) {
+	// call the specified method (passing along subsequent arguments) or 
+	// default to the "initialize" method (passing along all arguments)
+	$.fn.keyframeAnimation = function(methodOrSettings/* [, ...] */) {
+		if($.fn.keyframeAnimation[methodOrSettings]) {
+			return $.fn.keyframeAnimation[methodOrSettings].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else {
+			return $.fn.keyframeAnimation.initialize.apply(this, arguments);
+		}
+	};
+	
+	
+	// begin animation
+	$.fn.keyframeAnimation.initialize = function(settings) {
 		// TODO: document settings
+		// these correspond as closely as possible to CSS defaults
 		var defaultSettings = {
 			keyframes: {},
 			delays: [],
@@ -30,14 +43,22 @@
 		
 		var elements = this;
 		var orderedKeyframePercentages = Object.keys(settings.keyframes).sort();
-		var currentIteration = 0;
 		
+		// create a namespace for element data
+		if(elements.data('keyframeAnimation') === undefined) {
+			elements.data('keyframeAnimation', {});
+		}
+		
+		var currentIteration = 0;
 		var animate = function() {
 			currentIteration++;
 			if(settings.animationIterationCount !== 'infinite' && currentIteration > settings.animationIterationCount) {
 				// done animating
+				delete elements.data('keyframeAnimation').timeouts;
 				return;
 			} else {
+				elements.data('keyframeAnimation').timeouts = [];
+				
 				// for each element, iterate over each keyframe and set up 
 				// an animation with the appropriate delay (as determined 
 				// by the element delay and the keyframe percentage) and 
@@ -83,7 +104,7 @@
 						var interpolationDuration = settings.animationDuration * percentageBetweenKeyframes;
 						
 						// delay transition start as needed
-						window.setTimeout(function() {
+						elements.data('keyframeAnimation').timeouts.push(window.setTimeout(function() {
 							// is animation necessary?
 							if(interpolationDuration === 0) {
 								$(element).css(styles);
@@ -94,17 +115,32 @@
 									easing: makeEasingFunction(settings.animationTimingFunction)
 								});
 							}
-						}, keyframeOffset + elementDelay);
+						}, keyframeOffset + elementDelay));
 					});
-					
-					// run the next cycle after settings.animationDuration
-					window.setTimeout(animate, settings.animationDuration);
 				});
+				
+				// run the next cycle after `settings.animationDuration`
+				elements.data('keyframeAnimation').timeouts.push(window.setTimeout(animate, settings.animationDuration));
 			}
 		};
 		
 		// start the animation cycle (it will repeat itself)
 		animate();
+		
+		// allow chaining
+		return this;
+	};
+	
+	
+	// immediately clear all animation timers; note that in-progress 
+	// $(...).animate() transitions will not be halted
+	$.fn.keyframeAnimation.abort = function() {
+		if(this.data('keyframeAnimation').timeouts) {
+			$.each(this.data('keyframeAnimation').timeouts, function(index, timeoutID) {
+				window.clearTimeout(timeoutID);
+			});
+			delete this.data('keyframeAnimation').timeouts;
+		}
 		
 		// allow chaining
 		return this;
@@ -136,8 +172,14 @@
 	};
 	
 	
-	// create a $.easing function for an animationTimingFunction and return 
+	// create a $.easing method based on some timingFunctionPoints and return 
 	// its name
+	// FIXME? is the extra complexity of creating these on-the-fly worth the 
+	//        small performance savings and the smaller $.easing namespace 
+	//        footprint?  since $.easing is externally-visible, will it be 
+	//        confusing to not always have all css* easing functions present?
+	//        something like this would definitely still be necessary for 
+	//        custom timing functions (e.g. "cubic-bezier(x1, y1, x2, y2)")
 	var makeEasingFunction = function(timingFunctionName) {
 		// e.g. "cssEaseIn"
 		var easingName = 'css' + timingFunctionName.charAt(0).toUpperCase() + timingFunctionName.slice(1);
@@ -145,8 +187,7 @@
 		// $.fn.animate cannot accept easings as functions (lame), instead 
 		// they need to be put into $.easing and specified via property names
 		if($.easing[easingName] === undefined) {
-			// an easing function for this animationTimingFunction doesn't 
-			// already exist; create it
+			// create a new $.easing method
 			var points = timingFunctionPoints[timingFunctionName];
 			$.easing[easingName] = function(percentComplete, millisecondsSince, startValue, endValue, totalDuration) {
 				var totalDurationSeconds = totalDuration / 1000;
@@ -164,7 +205,14 @@
 	// http://www.netzgesta.de/dev/cubic-bezier-timing-function.html
 	// TODO: more descriptive variable names
 	var cubicBezierAtTime = function(t, p1x, p1y, p2x, p2y, duration) {
-		var ax = 0, bx = 0, cx = 0, ay = 0, by = 0, cy = 0;
+		// calculate the polynomial coefficients, implicit first and last 
+		// control points are (0,0) and (1,1)
+		var cx = 3 * p1x;
+		var bx = 3 * (p2x - p1x) - cx;
+		var ax = 1 - cx - bx;
+		var cy = 3 * p1y;
+		var by = 3 * (p2y - p1y) - cy;
+		var ay = 1 - cy - by;
 		
 		// `ax t^3 + bx t^2 + cx t` expanded using Horner's rule
 		var sampleCurveX = function(t) {
@@ -174,18 +222,7 @@
 			return ((ay * t + by) * t + cy) * t;
 		};
 		var sampleCurveDerivativeX = function(t) {
-			return (3.0 * ax * t + 2.0 * bx) * t + cx;
-		};
-		
-		// the epsilon value to pass given that the animation is going to run 
-		// over `duration` seconds.
-		// the longer the animation, the more precision is needed in the 
-		// timing function result to avoid ugly discontinuities.
-		var solveEpsilon = function(duration) {
-			return 1.0 / (200.0 * duration);
-		};
-		var solve = function(x, epsilon) {
-			return sampleCurveY(solveCurveX(x, epsilon));
+			return (3 * ax * t + 2 * bx) * t + cx;
 		};
 		
 		// given an x value, find a parametric value it came from
@@ -206,8 +243,8 @@
 			}
 			
 			// fall back to the bisection method for reliability
-			t0 = 0.0;
-			t1 = 1.0;
+			t0 = 0;
+			t1 = 1;
 			t2 = x;
 			if(t2 < t0) {
 				return t0;
@@ -230,18 +267,15 @@
 			return t2; // failure (FIXME: what does this imply?)
 		};
 		
-		// calculate the polynomial coefficients, implicit first and last 
-		// control points are (0,0) and (1,1)
-		cx = 3.0 * p1x;
-		bx = 3.0 * (p2x - p1x) - cx;
-		ax = 1.0 - cx - bx;
-		cy = 3.0 * p1y;
-		by = 3.0 * (p2y - p1y) - cy;
-		ay = 1.0 - cy - by;
+		// the epsilon value to pass given that the animation is going to run 
+		// over `duration` seconds.
+		// the longer the animation, the more precision is needed in the 
+		// timing function result to avoid ugly discontinuities.
+		var epsilon = 1 / (200 * duration);
 		
 		// convert from input time to parametric value in curve, then from 
 		// that to output time
-		return solve(t, solveEpsilon(duration));
+		return sampleCurveY(solveCurveX(t, epsilon));
 	};
 	
 }(jQuery, window));
